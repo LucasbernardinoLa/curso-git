9e3787c7c8056ee17dc4e37032dbf2fa4d85cf69
Lucas Bernardino <lucasblameu@gmail.com>
2021-12-26 12:25:59 -0300

    Projeto de teste usando gitbash

---
+++ projete-teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projete_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
